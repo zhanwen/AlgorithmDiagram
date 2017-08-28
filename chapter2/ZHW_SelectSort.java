48bb6f0f5c3a08ee7ce881e455efeddc15161aae
Hanwen <1106002609@qq.com>
2017-08-28 17:53:35 +0800

    SelectSort

---
+++ chapter2/ZHW_SelectSort.java
@@ -0,0 +1,34 @@
+
+/**
+ * 
+ * @author Hanwen
+ * @date 2017年8月28日 下午5:45:48
+ * @version
+ */
+public class SelectSort {
+	public static void main(String[] args) {
+		int[] array = new int[]{1,4,5,8,9,6,2,3,7,10};
+		int[] result = findMinNumber(array);
+		for(int i = 0; i < result.length; i++) {
+			System.out.print(result[i]+ " ");
+		}
+	}
+	
+	/**
+	 * 选择排序
+	 * @param array	数组
+	 * @return 排序好的数组(从小到大)
+	 */
+	private static int[] findMinNumber(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			for(int j = i+1; j < array.length; j++) {
+				if(array[i] > array[j]) {
+					int temp = array[j];
+					array[j] = array[i];
+					array[i] = temp;
+				}
+			}
+		}
+		return array;
+	}
+}
---
+++ chapter2/ZHW_SelectSort.py
@@ -0,0 +1,19 @@
+#查找数组中的最小数的索引
+def findMinNumber(array) :
+    min = array[0]
+    min_index = 0
+    for i in range(1, len(array)) :
+        if array[i] < min :
+            min = array[i]
+            min_index = i
+    return min_index
+
+#选择排序
+def selectSort(array) :
+    newArray = []
+    for i in range(len(array)) :
+        min = findMinNumber(array)
+        newArray.append(array.pop(min))
+    return newArray
+
+print(selectSort([4,2,5,6,7,9,1,8,3,10]))
